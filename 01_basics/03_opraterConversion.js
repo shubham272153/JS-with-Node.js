1753955621738cb6dbbe96ac0bb5f315ced285b7
shubham272153 <148613214+shubham272153@users.noreply.github.com>
2024-08-30 05:54:46 +0000

    operations

--- 01_basics/03_opraterConversion.js
+++ 01_basics/03_opraterConversion.js
@@ -1,18 +1,73 @@
-let score ="55abc"
+// let score ="55abc"
 
-console.log(typeof score);
+// console.log(typeof score);
 
-console.log( typeof(score));
+// console.log( typeof(score));
 
-let valueInNumber = Number(score)
+// let valueInNumber = Number(score)
 
-console.log(typeof valueInNumber);
+// console.log(typeof valueInNumber);
 
-console.log(valueInNumber);
+// console.log(valueInNumber);
 
 //"55" =>55
 //"55abc"=>NaN
 //true =>1; false=>0;
 //null=>objects
+
+//****************** operation ************ *//
+
+// let a= 10
+// let nevValue= -a
+
+// console.log(nevValue);
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2/2)
+// console.log(2%2)
+
+let str1 = "Hello"
+let str2 = " shubham"
+
+// let str3 = str1 + str2
+// console.log(str3);
+
+//** imp example **/
+                        // outputs of example
+// 2console.log(1 + 2)     //   3
+// console.log("1" + 2);  // 12
+// console.log(1 + "2");  // 12
+// console.log("1"+ 2 + 2); // 122
+// console.log(1 + 2 + "2")  // 32
+
+
+/*    ------   imp example ----------*/
+/*  { to cheack mdn documents for operaters}
+
+let a = 10
+ ++a;
+console.log(a)
+
+let b = 10
+ b++;
+console.log(b)
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
